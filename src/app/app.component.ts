521389997d5f858bcc5ceedae4dc4cc20b00dbc2
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-18 16:51:25 +0100

    feat: header with title in blue closes#3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ab-root',
-  template: ``,
-  styles: [],
+  template: `
+    <header>
+      <h1>{{ title }}</h1>
+      <p>Angular application example for lift off 🚀</p>
+    </header>
+  `,
+  styles: [
+    `
+      h1 {
+        color: #28aaff;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'angular-booster';
