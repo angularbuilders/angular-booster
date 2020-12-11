c1dfc699185e5b462459ec01d5325fba15a8207c
Alberto Basalo <albertobasalo@hotmail.com>
2020-12-11 19:03:41 +0100

    ng add @nguniversal/express-engine #18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
