166006c2d7b5f7c8de5237d878ab656b9775145e
Alberto Basalo <albertobasalo@hotmail.com>
2020-12-18 12:55:15 +0100

    fix: add core modules where needed

--- src/app/launch/launch.module.ts
+++ src/app/launch/launch.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { LaunchRoutingModule } from './launch-routing.module';
 import { LaunchComponent } from './launch.component';
 
 @NgModule({
   declarations: [LaunchComponent],
-  imports: [CommonModule, LaunchRoutingModule, SharedModule],
+  imports: [CommonModule, LaunchRoutingModule, SharedModule, CoreModule],
 })
 export class LaunchModule {}
